e7372a332c863b6f0ae53af13a8d40cd730b52d0
jaeyoung <jyp101400@naver.com>
2022-12-29 13:20:33 +0900

    Initial commit

---
+++ src/test/java/gdsc/yolog/YologApplicationTests.java
@@ -0,0 +1,13 @@
+package gdsc.yolog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YologApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
